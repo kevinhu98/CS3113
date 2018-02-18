f88faff0feb4bddaa36214f6141b09715d132a62
Kevin Hu <kh2547@nyu.edu>
2018-02-18 16:24:48 -0500

    finished hw1

--- Hw1/NYUCodebase/main.cpp
+++ Hw1/NYUCodebase/main.cpp
@@ -1,27 +1,98 @@
 #ifdef _WINDOWS
-	#include <GL/glew.h>
+#include <GL/glew.h>
 #endif
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <SDL_image.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "ShaderProgram.h"
 
 #ifdef _WINDOWS
-	#define RESOURCE_FOLDER ""
+#define RESOURCE_FOLDER ""
 #else
-	#define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
+#define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
 #endif
 
 SDL_Window* displayWindow;
 
+GLuint LoadTexture(const char *filePath) {
+	int w, h, comp;
+	unsigned char* image = stbi_load(filePath, &w, &h, &comp, STBI_rgb_alpha);
+
+	if (image == NULL) {
+		std::cout << "Unable to load image. Make sure the path is correct \n";
+		assert(false);
+	}
+
+	GLuint retTexture;
+	glGenTextures(1, &retTexture);
+	glBindTexture(GL_TEXTURE_2D, retTexture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	stbi_image_free(image);
+	return retTexture;
+}
+
+void DrawTexture(ShaderProgram& program, const Matrix& modelMatrix, GLuint texture) {
+	program.SetModelMatrix(modelMatrix);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+
+	glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+	glEnableVertexAttribArray(program.positionAttribute);
+
+	float texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+
+	glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+	glEnableVertexAttribArray(program.texCoordAttribute);
+
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+
+	glDisableVertexAttribArray(program.positionAttribute);
+	glDisableVertexAttribArray(program.texCoordAttribute);
+}
+
 int main(int argc, char *argv[])
 {
 	SDL_Init(SDL_INIT_VIDEO);
-	displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
+	displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL);
 	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
 	SDL_GL_MakeCurrent(displayWindow, context);
-	#ifdef _WINDOWS
-		glewInit();
-	#endif
+#ifdef _WINDOWS
+	glewInit();
+#endif
+	glViewport(0, 0, 1280, 720);
+
+	ShaderProgram program;
+	ShaderProgram untextured;
+	
+	program.Load(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
+	untextured.Load(RESOURCE_FOLDER"vertex.glsl", RESOURCE_FOLDER"fragment.glsl");
+	
+	GLuint frog = LoadTexture(RESOURCE_FOLDER"datboi.png");
+	GLuint sanic = LoadTexture(RESOURCE_FOLDER"sanic.png");
+
+	Matrix projectionMatrix;
+	Matrix modelMatrix;
+	Matrix viewMatrix;
+	projectionMatrix.SetOrthoProjection(-3.55, 3.55, -2.0f, 2.0f, -1.0f, 1.0f);
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	float lastFrameTicks = 0.0f;
+	float x1_position = -3.5;
+	float y2_position = 2.5;
+	
+	program.SetProjectionMatrix(projectionMatrix);
+	program.SetViewMatrix(viewMatrix);
+	untextured.SetProjectionMatrix(projectionMatrix);
+	untextured.SetViewMatrix(viewMatrix);
 
 	SDL_Event event;
 	bool done = false;
@@ -31,10 +102,43 @@ int main(int argc, char *argv[])
 				done = true;
 			}
 		}
+		//time
+		float ticks = (float)SDL_GetTicks() / 1000.0f;
+		float elapsed = ticks - lastFrameTicks;
+		lastFrameTicks = ticks;
+
 		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(untextured.programID);
+	
+		//triangle
+		modelMatrix.Identity();
+		untextured.SetModelMatrix(modelMatrix);
+		float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+		glVertexAttribPointer(untextured.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+		glEnableVertexAttribArray(untextured.positionAttribute);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDisableVertexAttribArray(untextured.positionAttribute);
+
+		glUseProgram(program.programID);
+
+		//frog + sonic
+		modelMatrix.Identity();
+		modelMatrix.Translate(x1_position, 0, 0);
+		DrawTexture(program, modelMatrix, frog);
+
+		modelMatrix.Identity();
+		modelMatrix.Translate(0, y2_position, 0);
+		DrawTexture(program, modelMatrix, sanic);
+		//transition sonic after frog
+		if(x1_position > 3.5) {
+			y2_position -= elapsed;
+		}
+		x1_position += elapsed;
+		
 		SDL_GL_SwapWindow(displayWindow);
 	}
-
 	SDL_Quit();
 	return 0;
 }
+
+
